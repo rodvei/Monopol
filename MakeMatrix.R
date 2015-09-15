6887ecb1135eb5b82d7c91fce5b237d9d8dd3e25
unknown <rodvei@hotmail.com>
2015-09-15 22:23:27 +0200

    Finished version 1

--- MakeMatrix.R
+++ MakeMatrix.R
@@ -15,7 +15,7 @@ for(i in 1:40){
 #Fengsel
 brettet[30,]=brettet[10,]*(5/6)^9+(1-(5/6)^9)*c(rep(0,11),1/6,0,1/6,0,1/6,0,1/6,0,1/6,0,1/6,rep(0,18))
 
-
+#prov lykken kort
 for(i in provlykkenloc){
   temp=which(brettet[,i]!=0)
   for(j in temp){
@@ -24,6 +24,7 @@ for(i in provlykkenloc){
   }
 }
 
+#sjanse kort
 for(i in sjanseloc){
   temp=which(brettet[,i]!=0)
   for(j in temp){
@@ -33,3 +34,27 @@ for(i in sjanseloc){
     brettet[j,i]=brettet[j,i]*8/16
   }
 }
+
+
+brettetn=brettet%*%brettet #brettetn,n=2
+brettetn=brettetn%*%brettetn #brettetn,n=4
+brettetn=brettetn%*%brettetn #brettetn,n=8
+brettetn=brettetn%*%brettetn #brettetn,n=16
+brettetn=brettetn%*%brettetn #brettetn,n=32
+brettetn=brettetn%*%brettetn #brettetn,n=64
+brettetn=brettetn%*%brettetn #brettetn,n=128
+
+plot(1:40, brettetn[1,],type='h',xaxt='n')
+axis(1, at = 1:40, las=2)
+
+
+
+#gate type
+tomt=list(brun=c(1,3), lblaa=c(6,8,9), rosa=c(11,13,14), orange=c(16,18,19), rod=c(21,23,24), gul=c(26,27,29), gronn=c(31,32,34),mblaa=c(37,39))
+name=c('brun','lblaa','rosa','orange','rod','gul','gronn','mblaa')
+tomtint=rep(0,8)
+for(i in 1:length(tomt)){
+  tomtint[i]=sum(brettetn[1,tomt[[i]]])/length(tomt[[i]])
+}
+plot(1:8,tomtint,type='h',xaxt='n')
+axis(1, 1:8, name)
